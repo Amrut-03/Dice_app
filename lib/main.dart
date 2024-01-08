dad579d5db5a6bec4bbdfb6c6814f4d86ce11286
Amrut-03 <amrutkhochikar@gmail.com>
2024-01-08 19:21:38 +0530

    finished

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        appBar: AppBar(
+          title: const Text(
+            "Dice App",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('assets/images/dice_$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('assets/images/dice_$rightDiceNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
